ce93e0d6e468c14c3d4d18530fb16a267589f3a0
Hoang <hnguyen0428@Hoangs-MacBook-Pro.local>
2017-04-01 10:13:39 -0700

    WIP

---
+++ age_guess.py
@@ -0,0 +1,12 @@
+import cognitive_face as CF
+import os
+
+KEY = '189b110e646145459cbdddd4d8f8d161'  # Replace with a valid subscription key (keeping the quotes in place).
+CF.Key.set(KEY)
+
+original_image = 'hoang.jpg'
+result = CF.face.detect(original_image, True, False, "age")
+
+age = result[0]['faceAttributes']
+
+print age
---
+++ face_match_request.py
@@ -0,0 +1,27 @@
+########### Python 2.7 #############
+import httplib, urllib, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.urlencode({
+    # Request parameters
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': '{string}',
+})
+
+body = "{'url':'hoang.jpg'}"
+
+try:
+    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
